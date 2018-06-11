9d5286df34251576ee0601ac1fbe8ffcc1f6c85a
jackmoody11 <jackmoody@unc.edu>
2018-06-10 22:04:24 -0400

    Add default url for score functions

--- test_technical_functions.py
+++ test_technical_functions.py
@@ -8,5 +8,5 @@ stock_scores = setup.init_stock_scores(symbols)
 
 
 def test_moving_avg():
-    scores = tf.moving_avg_test(batch_symbols, iex_url_base, stock_scores)
+    scores = tf.moving_avg_test(batch_symbols, stock_scores)
     assert len(scores) >= 100, 'At least 100 scores listed in stock_scores dictionary'
