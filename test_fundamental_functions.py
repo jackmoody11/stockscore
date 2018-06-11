42331f55bf65c0a3cfa26ba66e00f5b073de53d1
jackmoody11 <jackmoody@unc.edu>
2018-06-10 22:09:28 -0400

    Add fundamental functions tests

---
+++ test_fundamental_functions.py
@@ -0,0 +1,15 @@
+import fundamental_functions as ff
+import setup
+
+iex_url_base = "https://api.iextrading.com/1.0/"
+symbols = setup.get_symbols(iex_url_base)
+batch_symbols = setup.set_batches(symbols)
+stock_scores = setup.init_stock_scores(symbols)
+
+def test_dividend_test():
+    scores = ff.dividend_test(batch_symbols, stock_scores)
+    assert len(scores) >= 100, 'At least 100 dividend scores listed in stock_scores dictionary'
+
+def test_net_income_test():
+    scores = ff.net_income_test(batch_symbols, stock_scores)
+    assert len(scores) >= 100, 'At least 100 net income scores listed in stock_scores dictionary'
