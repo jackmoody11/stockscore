f86302679d783bc41438784a7cd4c6e2d945e992
jackmoody11 <jackmoody@unc.edu>
2018-06-24 16:34:31 -0400

    Reorganize functions and add manual capability for getting symbols

--- data_read.py
+++ data_read.py
@@ -1,25 +1,41 @@
+# Modules
 import openpyxl as xl
 import pandas as pd
 import datetime
 import requests
 
+# Variables
 iex_url_base = "https://api.iextrading.com/1.0/"
-file = 'stock_score_data.xlsx'
-wb = xl.load_workbook(file)
-ws = wb['Data']
 
-last_updated = ws['B1'].value
-time_now = datetime.datetime.now()
-time_diff = time_now - last_updated
 
-def get_symbols(iex_url_base = iex_url_base):
+# Functions
+def xl_get_symbols():
+
+    file = 'stock_score_data.xlsx'
+    wb = xl.load_workbook(file)
+    ws = wb['Data']
+    symbols = []
+
+    for i in range(4,6000):
+        if ws['A%s'%i].value != None:
+            symbols.append(ws['A%s'%i].value)
+    return symbols
+
+
+def get_symbols(iex_url_base = iex_url_base, manual = False):
 
     """Gets all symbols from IEX API (uppercase) or Excel
     if saved within last day. """
-    # if Excel has tickers:
-    # symbols = dr.get_xl_symbols()
-    # else
-    if time_diff > datetime.timedelta(days=5):
+
+    file = 'stock_score_data.xlsx'
+    wb = xl.load_workbook(file)
+    ws = wb['Data']
+
+    last_updated = ws['B1'].value
+    time_now = datetime.datetime.now()
+    time_diff = time_now - last_updated
+
+    if (last_updated == None or time_diff > datetime.timedelta(days=5) or manual):
 
         symbols_json = requests.get(iex_url_base + "ref-data/symbols").json()
         symbols = []
@@ -47,7 +63,7 @@ def xl_update_symbols():
     if (last_updated == None or time_diff > datetime.timedelta(days=5)):
         ws['B1'].value = datetime.datetime.now()
         last_updated = datetime.datetime.now()
-        symbols = get_symbols()
+        symbols = get_symbols(manual = True)
         for i in range(4,len(symbols)+4):
             ws['A%s'%i] = symbols[i - 4]
 
@@ -56,15 +72,5 @@ def xl_update_symbols():
     # print(ws['A10'].value)
 
 
-def xl_get_symbols():
 
-    file = 'stock_score_data.xlsx'
-    wb = xl.load_workbook(file)
-    ws = wb['Data']
-    symbols = []
-
-    for i in range(4,6000):
-        if ws['A%s'%i].value != None:
-            symbols.append(ws['A%s'%i].value)
-    return symbols
 
