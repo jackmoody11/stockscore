2ff685a886a46971c88f4d3b975404d7031d1b66
jackmoody11 <jackmoody@unc.edu>
2018-06-26 20:16:55 -0400

    Start thinking about OOP way to make tests easier

---
+++ stockScore/stock_scoring.py
@@ -0,0 +1,28 @@
+import requests
+
+class Stock:
+
+    iex_url_base = "https://api.iextrading.com/1.0/"
+
+    def __init__(self):
+
+        # Initialize different scores
+        self.total_score = 0
+        self.value_score = 0
+        self.growth_score = 0
+        self.momentum_score = 0
+        self.sentiment_score = 0
+
+        # Initialize different stock attributes
+        self.ticker =''
+        self.company_name = ''
+        self.current_price = None
+        self.pe_ratio = None
+        self.pb_ratio = None
+        self.market_cap = None
+
+        # Initialize DCF variables
+        self.wacc = None
+        self.cost_of_equity = None
+        self.cost_of_debt = None
+        self.dcf_fair_value = None
