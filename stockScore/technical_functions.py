00678c401f6a6982a19af781cf3bd475677db623
jackmoody11 <jackmoody@unc.edu>
2018-06-28 20:59:51 -0400

    Formatting by PEP8 Standards

--- stockScore/technical_functions.py
+++ stockScore/technical_functions.py
@@ -2,28 +2,32 @@ import requests
 iex_url_base = "https://api.iextrading.com/1.0/"
 
 
-def suite(batch_symbols, stock_scores, api_url_base = iex_url_base):
+def suite(batch_symbols, stock_scores, api_url_base=iex_url_base):
 
     stock_scores = moving_avg_test(batch_symbols, stock_scores)
     return stock_scores
 
-def moving_avg_test(batch_symbols, stock_scores, api_url_base = iex_url_base):
+
+def moving_avg_test(batch_symbols, stock_scores, api_url_base=iex_url_base):
 
     for i in batch_symbols:
-        batch_url = api_url_base + "stock/market/batch?symbols=" + batch_symbols[i] + "&types=stats"
+        batch_url = api_url_base + "stock/market/batch?symbols=" + \
+            batch_symbols[i] + "&types=stats"
         result = requests.get(batch_url).json()
         for symbol in result:
             base = result[symbol]['stats']
             if(base['day200MovingAvg'] and base['day50MovingAvg']):
                 avg_50 = base['day50MovingAvg']
                 avg_200 = base['day200MovingAvg']
-                per_diff = ((avg_50 - avg_200)/avg_200) * 100
+                per_diff = ((avg_50 - avg_200) / avg_200) * 100
                 score = per_diff
                 if(0 < per_diff < 2):
-                    stock_scores[symbol] += round(5/(score + 1))
-                    print(symbol + " score went up by " + str(round(5/(score+1))) + " -- SMA 200 under SMA 50 by " + str(per_diff) + "%")
+                    stock_scores[symbol] += round(5 / (score + 1))
+                    print(symbol + " score went up by " + str(round(5 / (score + 1))
+                                                              ) + " -- SMA 200 under SMA 50 by " + str(per_diff) + "%")
                 elif(2 < per_diff < 5):
-                    stock_scores[symbol] += round(5/score)
-                    print(symbol + " score went up by " + str(round(5/score)) + " -- SMA 200 under SMA 50 by " + str(per_diff) + "%")
+                    stock_scores[symbol] += round(5 / score)
+                    print(symbol + " score went up by " + str(round(5 / score)
+                                                              ) + " -- SMA 200 under SMA 50 by " + str(per_diff) + "%")
 
     return stock_scores
--- stockScore/wacc.py
+++ stockScore/wacc.py
@@ -8,82 +8,93 @@ in_pass = "1289904d544b7fe0cc14e21d6622e24c"
 
 
 def get_beta(symbol):
-	json = requests.get(iex_url_base + "/stock/" + symbol.lower() + "/stats").json()
-	if float(json['beta']):
-		beta = float(json['beta'])
-	else:
-		beta = None
-	return beta
+    json = requests.get(iex_url_base + "/stock/" +
+                        symbol.lower() + "/stats").json()
+    if float(json['beta']):
+        beta = float(json['beta'])
+    else:
+        beta = None
+    return beta
 
 
 def get_debt_to_equity(symbol):
 
-	json = requests.get(iex_url_base + "stock/" + symbol.lower() + "/financials").json()
-	if int(json['financials'][0]['totalDebt']) and int(json['financials'][0]['shareholderEquity']):
-		total_debt = int(json['financials'][0]['totalDebt'])
-		total_equity = int(json['financials'][0]['shareholderEquity'])
-		debt_to_equity = total_debt/total_equity
-	else:
-		debt_to_equity = None
+    json = requests.get(iex_url_base + "stock/" +
+                        symbol.lower() + "/financials").json()
+    if int(json['financials'][0]['totalDebt']) and int(json['financials'][0]['shareholderEquity']):
+        total_debt = int(json['financials'][0]['totalDebt'])
+        total_equity = int(json['financials'][0]['shareholderEquity'])
+        debt_to_equity = total_debt / total_equity
+    else:
+        debt_to_equity = None
 
-	return debt_to_equity
+    return debt_to_equity
 
 
 def get_rf():
 
-	rf_url = "https://fred.stlouisfed.org/series/DGS30"
-	rf_soup = start.soup_it(rf_url)
-	if float(rf_soup.find('span', attrs={'class':'series-meta-observation-value'}).text.strip()):
-		rf_rate = float(rf_soup.find('span', attrs={'class':'series-meta-observation-value'}).text.strip())
-		return rf_rate/100
-	else:
-		return None
+    rf_url = "https://fred.stlouisfed.org/series/DGS30"
+    rf_soup = start.soup_it(rf_url)
+    if float(rf_soup.find('span', attrs={'class': 'series-meta-observation-value'}).text.strip()):
+        rf_rate = float(rf_soup.find(
+            'span', attrs={'class': 'series-meta-observation-value'}).text.strip())
+        return rf_rate / 100
+    else:
+        return None
 
 
 def get_tax_rate_iex(symbol):
 
-	json = requests.get(iex_url_base + "stock/" + symbol.lower() + "/financials").json()
-	tax_rates = []
-	for i in range(0,4):
-		operating_income = int(json['financials'][i]['operatingIncome'])
-		net_income = int(json['financials'][i]['netIncome'])
-		taxes = operating_income - net_income
-		tax_rate = taxes/operating_income
-		tax_rates.append(tax_rate)
-	return np.mean(tax_rates) # Use numpy to get average tax rate for trailing twelve months
+    json = requests.get(iex_url_base + "stock/" +
+                        symbol.lower() + "/financials").json()
+    tax_rates = []
+    for i in range(0, 4):
+        operating_income = int(json['financials'][i]['operatingIncome'])
+        net_income = int(json['financials'][i]['netIncome'])
+        taxes = operating_income - net_income
+        tax_rate = taxes / operating_income
+        tax_rates.append(tax_rate)
+    # Use numpy to get average tax rate for trailing twelve months
+    return np.mean(tax_rates)
 
 
 def get_tax_rate(symbol):
 
-	in_json = requests.get(in_url_base + "/financials/standardized?identifier=" + symbol.upper() + "&statement=income_statement&fiscal_year=2017&fiscal_period=FY", auth=(in_user, in_pass)).json()
-	pretax_income = in_json['data'][11]['value']
-	taxes = in_json['data'][12]['value']
-	tax_rate = taxes/pretax_income
-	return tax_rate
+    in_json = requests.get(in_url_base + "/financials/standardized?identifier=" + symbol.upper() +
+                           "&statement=income_statement&fiscal_year=2017&fiscal_period=FY", auth=(in_user, in_pass)).json()
+    pretax_income = in_json['data'][11]['value']
+    taxes = in_json['data'][12]['value']
+    tax_rate = taxes / pretax_income
+    return tax_rate
 
 
 def get_interest_exp(symbol):
 
-	in_json = requests.get(in_url_base + "/financials/reported?identifier=" + symbol.upper() + "&statement=income_statement&fiscal_year=2015&fiscal_period=FY", auth=(in_user, in_pass)).json()
-	data = in_json['data']
-	int_exp_dict = next((item for item in data if item["xbrl_tag"] == "InterestExpense"), None)
-	if(int_exp_dict):
-		interest_expense = int_exp_dict['value']
-	else:
-		interest_expense = 0.05 # Will need to find another way to get interest expense if not available
-	return interest_expense
+    in_json = requests.get(in_url_base + "/financials/reported?identifier=" + symbol.upper() +
+                           "&statement=income_statement&fiscal_year=2015&fiscal_period=FY", auth=(in_user, in_pass)).json()
+    data = in_json['data']
+    int_exp_dict = next((item for item in data if item[
+                        "xbrl_tag"] == "InterestExpense"), None)
+    if(int_exp_dict):
+        interest_expense = int_exp_dict['value']
+    else:
+        # Will need to find another way to get interest expense if not
+        # available
+        interest_expense = 0.05
+    return interest_expense
 
 
 # def getPretaxIncome(symbol):
 
 def get_wacc(symbol, rp):
 
-	debt_to_equity = get_debt_to_equity(symbol)
-	beta = get_beta(symbol)
-	risk_free = get_rf()
-	e_to_v = 1/(1+debt_to_equity)
-	d_to_v = 1/(1+(1/debt_to_equity))
-	cost_of_equity = risk_free + beta * rp
-	tax_rate = get_tax_rate(symbol)
-	WACC = e_to_v * cost_of_equity + d_to_v * (1 - tax_rate) * get_interest_exp(symbol)#/getPretaxIncome(symbol)
-	return WACC
+    debt_to_equity = get_debt_to_equity(symbol)
+    beta = get_beta(symbol)
+    risk_free = get_rf()
+    e_to_v = 1 / (1 + debt_to_equity)
+    d_to_v = 1 / (1 + (1 / debt_to_equity))
+    cost_of_equity = risk_free + beta * rp
+    tax_rate = get_tax_rate(symbol)
+    WACC = e_to_v * cost_of_equity + d_to_v * \
+        (1 - tax_rate) * get_interest_exp(symbol)  # /getPretaxIncome(symbol)
+    return WACC
