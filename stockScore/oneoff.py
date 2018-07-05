114967b0eb51cdbb1bbfe8a0cfdcc9936bf676e6
jackmoody11 <jackmoody@unc.edu>
2018-07-05 18:34:15 -0400

    Update technical test to work with multiprocessing and updated tests

--- stockScore/oneoff.py
+++ stockScore/oneoff.py
@@ -1,34 +1,4 @@
-from stockScore import start
-
-
-def net_income_test(batch_data, stock_scores):
-
-    """Adds 1 point to all stocks that have paid dividends the past four years.
-
-    Must already have stock_scores dictionary. To add this test, run stock_scores = ff.dividend_test(...)."""
-    # Get data through multiprocessing
-    pool_outputs = start.get_pool_response(batch_data, "&types=financials&range=5y")
-    for first in pool_outputs:
-        for batch in first:
-            for ni_json in batch:
-                for symbol in ni_json:
-                    if ni_json[symbol]['financials']['financials'] is not None:
-                        base = ni_json[symbol]['financials']['financials']
-                        base_length = len(base)
-                        if all(base[i]['netIncome'].is_numeric() for i in range(0, base_length)) \
-                                and all(base[j]['netIncome'] > 0 for j in range(0, base_length)):
-                            stock_scores[symbol] += base_length
-                            print(symbol + " score went up by " + str(
-                                base_length) + " -- positive net income for the last " + str(base_length) + " years")
-                        elif base[0]['netIncome'].is_numeric() and base[0]['netIncome'] > 0:
-                            stock_scores[symbol] += 1
-                            print(symbol + " score went up by 1 -- positive net income last year")
-
-    return stock_scores
-
-
-symbols = start.get_symbols()
-batches = start.set_batches(symbols)
-stock_scores = start.init_stock_scores(symbols)
-stock_scores = net_income_test(batches, stock_scores)
-print(stock_scores)
+# Use this for one off calculations/easy testing without making modules dirty
+# You may want to use these modules
+# from stockScore import start
+# import time
--- stockScore/technical_functions.py
+++ stockScore/technical_functions.py
@@ -1,4 +1,4 @@
-import requests
+from stockScore import start
 iex_url_base = "https://api.iextrading.com/1.0/"
 
 
@@ -8,26 +8,30 @@ def suite(batch_symbols, stock_scores):
     return stock_scores
 
 
-def moving_avg_test(batch_symbols, stock_scores, api_url_base=iex_url_base):
+def moving_avg_test(batch_data, stock_scores):
 
-    for i in batch_symbols:
-        batch_url = api_url_base + "stock/market/batch?symbols=" + \
-            batch_symbols[i] + "&types=stats"
-        result = requests.get(batch_url).json()
-        for symbol in result:
-            base = result[symbol]['stats']
-            if base['day200MovingAvg'] and base['day50MovingAvg']:
-                avg_50 = base['day50MovingAvg']
-                avg_200 = base['day200MovingAvg']
-                per_diff = ((avg_50 - avg_200) / avg_200) * 100
-                score = per_diff
-                if 0 < per_diff < 2:
-                    stock_scores[symbol] += round(5 / (score + 1))
-                    print(symbol + " score went up by " + str(round(5 / (score + 1))
-                                                              ) + " -- SMA 200 under SMA 50 by " + str(per_diff) + "%")
-                elif 2 < per_diff < 5:
-                    stock_scores[symbol] += round(5 / score)
-                    print(symbol + " score went up by " + str(round(5 / score)
-                                                              ) + " -- SMA 200 under SMA 50 by " + str(per_diff) + "%")
+    """ The Moving Avg test aims to find stocks that are gaining momentum by recognizing when the
+    the 50 day moving avg is just beginning to pass the 200 day moving avg. """
+    pool_outputs = start.get_pool_response(batch_data, "&types=stats")
+    for first in pool_outputs:
+        for batch in first:
+            for ma_json in batch:
+                for symbol in ma_json:
+                    base = ma_json[symbol]['stats']
+                    if base.get('day200MovingAvg') and base.get('day50MovingAvg'):
+                        avg_50 = base['day50MovingAvg']
+                        avg_200 = base['day200MovingAvg']
+                        per_diff = ((avg_50 - avg_200) / avg_200) * 100
+                        score = per_diff
+                        if 0 < per_diff < 2:
+                            pts = round(5 / (score + 1))
+                            stock_scores[symbol] += pts
+                            print(symbol + " score went up by " + str(pts) + " -- SMA 200 under SMA 50 by " + str(
+                                per_diff) + "%")
+                        elif 2 < per_diff < 5:
+                            pts = round(5 / score)
+                            stock_scores[symbol] += pts
+                            print(symbol + " score went up by " + str(pts) + " -- SMA 200 under SMA 50 by " + str(
+                                per_diff) + "%")
 
     return stock_scores
