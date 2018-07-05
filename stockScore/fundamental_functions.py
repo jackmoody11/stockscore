4674fe20b21932c1735ddf217ea73ba6412f1117
jackmoody11 <jackmoody@unc.edu>
2018-07-05 18:34:15 -0400

    Update technical test to work with multiprocessing and updated tests

--- stockScore/fundamental_functions.py
+++ stockScore/fundamental_functions.py
@@ -1,4 +1,3 @@
-import requests
 from stockScore import start
 iex_url_base = "https://api.iextrading.com/1.0/"
 
@@ -22,21 +21,29 @@ def dividend_test(batch_data, stock_scores):
     return stock_scores
 
 
-def net_income_test(batch_symbols, stock_scores, api_url_base=iex_url_base):
+def net_income_test(batch_data, stock_scores):
 
-    for i in batch_symbols:
-        batch_url = api_url_base + "stock/market/batch?symbols=" + batch_symbols[i] + "&types=financials&range=5y"
-        result = requests.get(batch_url).json()
-        for symbol in result:
-            if result[symbol.upper()].get('financials'):
-                base = result[symbol]['financials']['financials']
-                base_length = len(base)
-                if all(base[i]['netIncome'] for i in range(0, base_length)):
-                    stock_scores[symbol] += base_length
-                    print(symbol + " score went up by " + str(base_length) + " -- positive net income for the last " + str(base_length) + " years")
-                elif base[0]['netIncome']:
-                    stock_scores[symbol] += 1
-                    print(symbol + " score went up by 1 -- positive net income last year")
+    """Adds 1 point to all stocks that have paid dividends the past four years.
+
+    Must already have stock_scores dictionary. To add this test, run stock_scores = ff.dividend_test(...)."""
+    # Get data through multiprocessing
+    pool_outputs = start.get_pool_response(batch_data, "&types=financials&range=5y")
+    for first in pool_outputs:
+        for batch in first:
+            for ni_json in batch:
+                for symbol in ni_json:
+                    if ni_json[symbol].get('financials').get('financials') is not None:
+                        base = ni_json[symbol]['financials']['financials']
+                        base_length = len(base)
+                        if all(base[i]['netIncome'] for i in range(0, base_length)) \
+                                and all(base[j]['netIncome'] > 0 for j in range(0, base_length)):
+                            stock_scores[symbol] += base_length
+                            print(symbol + " score went up by " + str(
+                                base_length) + " -- positive net income for the last " + str(base_length) + " years")
+                        elif base[0]['netIncome'] and base[0]['netIncome'] > 0:
+                            stock_scores[symbol] += 1
+                            print(symbol + " score went up by 1 -- positive net income last year")
+
     return stock_scores
 
 
--- tests/test_fundamental_functions.py
+++ tests/test_fundamental_functions.py
@@ -3,18 +3,30 @@ from stockScore import start as start
 
 symbols = start.get_symbols()
 batch_symbols = start.set_batches(symbols)
-stock_scores = start.init_stock_scores(symbols)
 
 
 def test_dividend_test_returns_scores():
 
-    scores = ff.dividend_test(batch_symbols, stock_scores)
+    scores = start.init_stock_scores(symbols)
+    scores = ff.dividend_test(batch_symbols, scores)
     assert len(
         scores) >= 1000, 'At least 1000 dividend scores listed in stock_scores dictionary'
 
 
-def test_net_income_test():
+def test_net_income_test_returns_dictionary():
 
-    scores = ff.net_income_test(batch_symbols, stock_scores)
+    scores = start.init_stock_scores(symbols)
+    scores = ff.net_income_test(batch_symbols, scores)
     assert len(
         scores) >= 1000, 'At least 1000 net income scores listed in stock_scores dictionary'
+
+
+def test_net_income_test_returns_aapl_with_pos_ni_for_all_years_given():
+    """
+    This test assumes that AAPL will continue to have positive net income for years to come.
+    May (hopefully not) need to be updated in the future. Also assumes that 5y period is used
+    for Net Income test.
+    """
+    scores = start.init_stock_scores(symbols)
+    scores = ff.net_income_test(batch_symbols, scores)
+    assert scores['AAPL'] == 4
