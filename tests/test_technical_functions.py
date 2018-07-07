717a368a015b43ad6b665d93a14059121528b3ce
jackmoody11 <jackmoody@unc.edu>
2018-07-06 22:37:41 -0400

    Update tests

--- tests/test_technical_functions.py
+++ tests/test_technical_functions.py
@@ -1,14 +1,20 @@
 from stockScore import technical_functions as tf
 from stockScore import start as start
 
-iex_url_base = "https://api.iextrading.com/1.0/"
 symbols = start.get_symbols()
 batch_symbols = start.set_batches(symbols)
 
 
-def test_moving_avg():
+def test_moving_avg_returns_scores():
 
     scores = start.init_stock_scores(symbols)
     scores = tf.moving_avg_test(batch_symbols, scores)
     assert len(
-        scores) >= 100, 'At least 100 moving avg scores listed in stock_scores dictionary'
+        scores) >= 1000, 'At least 1000 moving avg scores listed in stock_scores dictionary'
+
+
+def test_moving_avg_not_all_zero():
+
+    scores = start.init_stock_scores(symbols)
+    scores = tf.moving_avg_test(batch_symbols, scores)
+    assert not all(score == 0 for score in scores.values())
