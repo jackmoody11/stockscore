560fe4f526c0776e41de51f46138b3d8927130e3
jackmoody11 <jackmoody@unc.edu>
2018-07-21 13:58:34 -0400

    Update functions with new data setting and handle exceptions better

--- stockScore/fundamental_functions.py
+++ stockScore/fundamental_functions.py
@@ -16,73 +16,80 @@ def dividend_test(batch_data, stock_scores):
     # Get data through multiprocessing
     pool_outputs = start.get_pool_response(batch_data, "&types=dividends&range=5y")
 
-    for first in pool_outputs:
-        for batch in first:
-            for div_json in batch:
-                for symbol in div_json:
-                    if div_json[symbol].get('dividends'):
-                        stock_scores[symbol] += 1
+    for batch in pool_outputs:
+        for div_json in batch:
+            for symbol in div_json:
+                if div_json[symbol].get('dividends'):
+                    stock_scores[symbol] += 1
 
     return stock_scores
 
 
-def net_income_test(batch_data, stock_scores):
+def net_income_test(batch_data, stock_scores, financials=None):
     """
     :param batch_data: List of concatenated symbols -- use get_symbols() and set_batches()
     functions to set batch_data
     :param stock_scores: Dictionary with stock symbols and corresponding scores
     (ex: {'AAPL': 5, 'FB': 7, 'TSLA': 1, 'TJX': 12}
+    :param financials: Dictionary with all financial information from IEX API (see get_financials in start
+    module for more info.
     :return: Returns an updated stock_score dictionary. Make sure to set stock_score to the function
     so that net_income_test() can return updated stock scores.
     """
-    # Get data through multiprocessing
-    pool_outputs = start.get_pool_response(batch_data, "&types=financials")
-    for first in pool_outputs:
-        for batch in first:
-            for ni_json in batch:
-                for symbol in ni_json:
-                    if ni_json[symbol].get('financials').get('financials') is not None:
-                        base = ni_json[symbol]['financials']['financials']
-                        base_length = len(base)
-                        if all(base[i]['netIncome'] for i in range(0, base_length)) \
-                                and all(base[j]['netIncome'] > 0 for j in range(0, base_length)):
-                            stock_scores[symbol] += base_length
-                            print(symbol + " score went up by " + str(
-                                base_length) + " -- positive net income for the last " + str(base_length) + " years")
-                        elif base[0]['netIncome'] and base[0]['netIncome'] > 0:
-                            stock_scores[symbol] += 1
-                            print(symbol + " score went up by 1 -- positive net income last year")
+    if financials is None:
+        financials = start.get_financials(batch_data)
+    for symbol in financials:
+        try:
+            symbol_financials = financials[symbol]['financials']['financials']
+            base_length = len(symbol_financials)
+            try:
+                if all(symbol_financials[i]['netIncome'] for i in range(0, base_length)) \
+                        and all(symbol_financials[j]['netIncome'] > 0
+                                for j in range(0, base_length)):
+                    stock_scores[symbol] += base_length
+                    print(symbol + " score went up by " + str(
+                        base_length) + " -- positive net income for the last " + str(base_length) + " years")
+                elif symbol_financials[0]['netIncome'] and \
+                        symbol_financials[0]['netIncome'] > 0:
+                    stock_scores[symbol] += 1
+                    print(symbol + " score went up by 1 -- positive net income last year")
+            except (KeyError, TypeError):
+                continue
+        except (KeyError, TypeError):
+            continue
 
     return stock_scores
 
 
-def current_ratio_test(batch_data, stock_scores):
+def current_ratio_test(batch_data, stock_scores, financials=None):
     """
     :param batch_data: List of concatenated symbols -- use get_symbols() and set_batches()
     functions to set batch_data
     :param stock_scores: Dictionary with stock symbols and corresponding scores
     (ex: {'AAPL': 5, 'FB': 7, 'TSLA': 1, 'TJX': 12}
+    :param financials: Dictionary with all financial information from IEX API (see get_financials in start
+    module for more info.
     :return: Returns an updated stock_score dictionary. Make sure to set stock_score to the function
     so that current_ratio() can return updated stock scores.
     """
-    pool_outputs = start.get_pool_response(batch_data, "&types=financials")
-    for first in pool_outputs:
-        for batch in first:
-            for ni_json in batch:
-                for symbol in ni_json:
-                    if ni_json[symbol].get('financials').get('financials') is not None:
-                        base = ni_json[symbol]['financials']['financials']
-                        if base[0]['currentAssets'] and base[0]['currentDebt']:
-                            current_assets = base[0]['currentAssets']
-                            current_debt = base[0]['currentDebt']
-                            if current_debt != 0:
-                                current_ratio = current_assets/current_debt
-                                if current_ratio >= 1.5:
-                                    stock_scores[symbol] += 2
-                                    print(symbol + " score went up by 2 -- current ratio >= 1.5")
-                                elif current_ratio >= 1:
-                                    stock_scores[symbol] += 1
-                                    print(symbol + " score went up by 1 -- current ratio >= 1")
+    if financials is None:
+        financials = start.get_financials(batch_data)
+    for symbol in stock_scores:
+        # Need to clean up with some sort of equivalent to dig() in Ruby
+        if financials.get(symbol) and financials[symbol]['financials'] and \
+           financials[symbol]['financials']['financials'] and financials[symbol]['financials']['financials'][0]:
+
+            current_assets = financials[symbol]['financials']['financials'][0]['currentAssets']
+            current_debt = financials[symbol]['financials']['financials'][0]['currentDebt']
+
+            if current_debt != 0 and (current_debt and current_assets) is not None:
+                current_ratio = current_assets/current_debt
+                if current_ratio >= 1.5:
+                    stock_scores[symbol] += 2
+                    print(symbol + " score went up by 2 -- current ratio >= .5")
+                elif current_ratio >= 1:
+                    stock_scores[symbol] += 1
+                    print(symbol + " score went up by 1 -- current ratio >= 1")
 
     return stock_scores
 
