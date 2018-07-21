eb3120d1c0de049fc8617e9246c0d5fdeb51f4ac
jackmoody11 <jackmoody@unc.edu>
2018-07-21 15:01:45 -0400

    Update testing

--- tests/test_statistical_functions.py
+++ tests/test_statistical_functions.py
@@ -1,8 +1,7 @@
 from stockScore import statistical_functions as sf
 from stockScore import start as start
 
-symbols = start.get_symbols()
-batch_data = start.set_batches(symbols)
+symbols, _, batch_data = start.total_setup()
 stats = start.get_stats(batch_data)
 chart = start.get_chart(batch_data)
 
@@ -30,4 +29,16 @@ def test_p_to_b_test_returns_scores():
 def test_p_to_b_not_all_zero():
     scores = start.init_stock_scores(symbols)
     scores = sf.p_to_b_test(batch_data, scores)
+    assert not all(score == 0 for score in scores.values())
+
+
+def test_suite_returns_scores():
+    scores = start.init_stock_scores(symbols)
+    scores = sf.suite(batch_data, scores, chart=chart, stats=stats)
+    assert len(scores) >= 1000, 'At least 1000 scores for suite test'
+
+
+def test_suite_not_all_zero():
+    scores = start.init_stock_scores(symbols)
+    scores = sf.suite(batch_data, scores, chart=chart, stats=stats)
     assert not all(score == 0 for score in scores.values())
