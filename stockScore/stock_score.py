15a49258009a6db12e4e3caaae85beeffd613b8d
jackmoody11 <jackmoody@unc.edu>
2018-07-22 21:43:15 -0400

    Update stock score with ff suite

--- stockScore/stock_score.py
+++ stockScore/stock_score.py
@@ -11,21 +11,27 @@ from stockScore import fundamental_functions as ff
 import time
 
 start_time = time.time()
+
 print('Setting up stocks and scores...')
 symbols, stock_scores, batch_data = start.total_setup()
+
 print('Fetching data dictionaries...')
-chart, stats, financials, splits = start.get_chart(batch_data), start.get_stats(batch_data), \
-                                   start.get_financials(batch_data), start.get_splits(batch_data)
+chart, stats, financials, splits, dividends = start.get_chart(batch_data), start.get_stats(batch_data), \
+                                              start.get_financials(batch_data), start.get_splits(batch_data), \
+                                              start.get_dividends(batch_data)
+
 print('Running tests...')
-stock_scores = ff.suite(batch_data, stock_scores, financials=financials)
-del financials
+stock_scores = ff.suite(batch_data, stock_scores, dividends=dividends, financials=financials)
+del financials, dividends
 stock_scores = sf.suite(batch_data, stock_scores, stats=stats, chart=chart)
 del chart
 stock_scores = tf.suite(batch_data, stock_scores, stats=stats, splits=splits)
 del stats, splits
 
-top_20 = start.return_top(stock_scores, 20)
 
 end_time = time.time()
+
+
+top_20 = start.return_top(stock_scores, 20)
 print("The top 20 companies are", top_20)
 print("That took " + str(end_time - start_time) + " seconds")
