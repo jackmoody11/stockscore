a7de9e4668771a54b810b0f75d64a28733275e06
jackmoody11 <jackmoody@unc.edu>
2018-08-04 10:03:30 -0400

    Add Travis CI

--- stockScore/oneoff.py
+++ stockScore/oneoff.py
@@ -1,9 +1,16 @@
-# Use this for one off calculations/easy testing without making modules dirty
-# You may want to use these modules:
+# # Use this for one off calculations/easy testing without making modules dirty
+# # You may want to use these modules:
 # from stockScore import start
-# import time
-
-
-# Will probably want these for setting up stockScore tests
+# import iexfinance
+# import pandas as pd
+# import datetime
+# # import time
+#
+#
+# # Will probably want these for setting up stockScore tests
 # symbols = start.get_symbols()
 # batches = start.set_batches(symbols)
+#
+# df = pd.DataFrame()
+# t1 = datetime.datetime(2016, 1, 1)
+# t2 = datetime.datetime(2018, 1, 1)
